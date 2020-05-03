746cfed5290aa4ccfee3b5862df6d4b74312ac0a
Ramu <bramu.ss@gmail.com>
2020-05-03 10:59:04 +0530

    changes

--- api/controllers/CategoryPageController.js
+++ api/controllers/CategoryPageController.js
@@ -1,23 +1,23 @@
 const _ = require('lodash');
 
 const CategoryPageController = () => {
-  
-  const show = async (req, res) => {
 
-      try {
-        let model = {};
-        model.layout = 'layouts/category.ejs';
-        return res.render('pages/category-page.ejs',model);
-      } catch (err) {
-        // console.log(err);
-        return res.status(500).json({ msg: 'Internal server error' });
-      }
-  };
+    const show = async(req, res) => {
+
+        try {
+            let model = {};
+            model.layout = 'layouts/category.ejs';
+            return res.render('pages/category-page.ejs', model);
+        } catch (err) {
+            // console.log(err);
+            return res.status(500).json({ msg: 'Internal server error' });
+        }
+    };
 
 
-  return {
-    show
-  };
+    return {
+        show
+    };
 };
 
 module.exports = CategoryPageController;
